bd52ed879ed9e53c6888880d5c2391689b3d2e8c
obama <obama110@163.com>
2025-03-22 00:49:09 +0800

    新增商人

--- statics/js/merchant-data.js
+++ statics/js/merchant-data.js
@@ -5,19 +5,26 @@ const merchantData = {
         {
             type: "丁香花商人",
             players: [
-                { id: "萌小只", uid: "1773644104", note: "", leaveTime: "3.21号20:24" }
+                { id: "萌小只", uid: "1773644104", note: "", leaveTime: "3.21号20:24" },
+                { id: "上岸小海苔", uid: "387364531", note: "无法搜索", leaveTime: "3.21号20:24" }
             ]
         },
         {
             type: "粉木菊商人",
             players: [
-                { id: "留白aD0", uid: "1000310657", note: "（无需艾特）", leaveTime: "3.22号11.37" }
+                { id: "留白aD0", uid: "1000310657", note: "无需艾特", leaveTime: "3.22号11.37" }
             ]
         },
         {
             type: "红色虞美人商人",
             players: [
-                { id: "圆圆呼吸硬糖", uid: "1547059951", note: "（无需艾特）", leaveTime: "3.22号11.40" }
+                { id: "圆圆呼吸硬糖", uid: "1547059951", note: "无需艾特", leaveTime: "3.22号11.40" }
+            ]
+        },
+                {
+            type: "樱花商人",
+            players: [
+                { id: "杨七柒", uid: "1168096522", note: "", leaveTime: "3.23号23.00" }
             ]
         }
     ]
