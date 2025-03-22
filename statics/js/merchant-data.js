3342600e1bb12c0e7944d812f8243d26376b2d1f
obama <obama110@163.com>
2025-03-22 14:13:49 +0800

    修改双倍商人目录

--- statics/js/merchant-data.js
+++ statics/js/merchant-data.js
@@ -5,20 +5,8 @@ const merchantData = {
         {
             type: "丁香花",
             players: [
-                { id: "萌小只", uid: "1773644104", note: "", leaveTime: "3.21号20:24" },
-                { id: "上岸小海苔", uid: "387364531", note: "无法搜索", leaveTime: "3.21号20:24" }
-            ]
-        },
-        {
-            type: "粉木菊",
-            players: [
-                { id: "留白aD0", uid: "1000310657", note: "无需艾特", leaveTime: "3.22号11.37" }
-            ]
-        },
-        {
-            type: "红色虞美人",
-            players: [
-                { id: "圆圆呼吸硬糖", uid: "1547059951", note: "无需艾特", leaveTime: "3.22号11.40" }
+                { id: "萌小只", uid: "1773644104", note: "", leaveTime: "3.23号20:24" },
+                { id: "上岸小海苔", uid: "387364531", note: "无法搜索", leaveTime: "3.23号20:24" }
             ]
         },
         {
