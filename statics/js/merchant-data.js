e1906ce030fbc30c7ebfa956ffefecf8dc15fd75
obama <obama110@163.com>
2025-03-24 12:26:09 +0800

    更新商人

--- statics/js/merchant-data.js
+++ statics/js/merchant-data.js
@@ -8,17 +8,6 @@ const merchantData = {
                 { id: "杏丝", uid: "524492413", note: "", leaveTime: "3.25号17.30" }
             ]
         },
-        {
-            type: "樱花",
-            players: [
-                { id: "杨七柒", uid: "1168096522", note: "", leaveTime: "3.23号23.00" },
-            ]
-        },
-        {
-            type: "月季",
-            players: [
-                { id: "KAYO", uid: "1112366727", note: "@黒玫", leaveTime: "3.24号10.43" }
-            ]
-        }
+
     ]
 }; 
